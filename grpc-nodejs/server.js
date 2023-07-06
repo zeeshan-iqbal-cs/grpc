f5ec80d32ca180e80c2830ea41f8231ca5fe9f56
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2023-07-06 17:24:00 +0500

    grpc-nodejs added

---
+++ grpc-nodejs/server.js
@@ -0,0 +1,36 @@
+const grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+
+const PROTO_PATH = "./news.proto";
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const newsProto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+let news = [
+  { id: "1", title: "Note 1", body: "Content 1", postImage: "Post image 1" },
+  { id: "2", title: "Note 2", body: "Content 2", postImage: "Post image 2" },
+];
+
+server.addService(newsProto.NewsService.service, {
+  getAllNews: (_, callback) => {
+    callback(null, { news: news });
+  },
+});
+
+server.bindAsync(
+  "127.0.0.1:50051",
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+    console.log("Server running at http://127.0.0.1:50051");
+    server.start();
+  }
+);
